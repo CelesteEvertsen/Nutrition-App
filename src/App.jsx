f7e8845b00fa95c7183f97ce9078efeecbc0a1b8
CelesteEvertsen <evertsen.celeste@gmail.com>
2026-03-10 02:41:44 +0100

    fjernet React icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 
-import { GiVanillaFlower } from "react-icons/gi";
+
 import './index.css'
 import NutritionFact from "./component/NutritionFact";
 function App() {
 
   return (
     <>
-      <GiVanillaFlower className='vanilla' />
     <div className="app">
       <NutritionFact/>
-     {/*  <span className='first'></span>
-      <span className='second'></span>
-      <span className='third'></span>
-      <span className='fourth'></span>
-      <span className='fifth'></span>
-      <span className='sixth'></span> */}
     </div>
    
     </>
